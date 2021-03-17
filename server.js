db58c73f7807529c46bac2dbeb4eb8fee02ac53f
Nickolaj <nickolaj99@outlook.com>
2021-03-17 15:46:19 +0100

    first commit

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const passport =  require("passport");
+
+const LocalStrategy =  require("passport-local");
+const passportLocalMongoose =  require("passport-local-mongoose");
+const User =  require("./models/user");
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/author')
@@ -22,7 +28,7 @@ app.use('/', indexRouter)
 app.use('/author', authorRouter)
 app.use('/books', bookRouter)
 
-const mongoose = require('mongoose')
+
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -32,3 +38,60 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'))
 app.listen(process.env.PORT || 3000)
 
+
+app.use(require("express-session")({
+    secret:"Any normal Word",       //decode or encode session
+    resave: false,
+    saveUninitialized:false,
+}));
+
+
+passport.serializeUser(User.serializeUser());       //session encoding
+passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get("/", (req,res) =>{
+    res.render("index");
+})
+app.get("/userprofile",isLoggedIn ,(req,res) =>{
+    res.render("userprofile");
+})
+//Auth Routes
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/userprofile",
+    failureRedirect:"/login"
+}),function (req, res){
+});
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+app.post("/register",(req,res)=>{
+
+    User.register(new User({username: req.body.username,phone:req.body.phone,telephone: req.body.telephone}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        }
+    passport.authenticate("local")(req,res,function(){
+        res.redirect("/login");
+    })
+    console.log(user)
+    })
+})
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+function isLoggedIn(req,res,next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
